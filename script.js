9236d29ea9cc85c1d95ab80f7ede1b777b55f26d
Joel/Zakloni <edjovilellaca@ittepic.edu.mx>
2024-09-06 09:54:30 -0700

    actualizado JS

--- script.js
+++ script.js
@@ -5,5 +5,7 @@ boton.addEventListener('click', ()=>{
     let num2 = Math.floor(Math.random() * 256);
     let num3 = Math.floor(Math.random() * 256);
 
-    document.body.style.backgroundColor(rgb(num1, num2, num3));
+    var colorazo = 'rgb(' + num1 + ',' + num2 + ',' + num3 + ')';
+
+    document.body.style.backgroundColor = colorazo;
 });
